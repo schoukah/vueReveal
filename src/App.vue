c5950d31c3b4b9f0a8f4e089ffc8626438e0200e
schoukah <21149759+schoukah@users.noreply.github.com>
2021-07-18 10:33:00 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div class="reveal">
+      <div class="slides">
+        <section>Single Horizontal Slide</section>
+        <section>
+          <section>Vertical Slide 1</section>
+          <section>Vertical Slide 2</section>
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Reveal from "reveal.js";
+
+export default {
+  name: "app",
+  components: {
+    // HelloWorld
+  },
+  mounted() {
+    Reveal.initialize();
+  },
+};
+</script>
+
+<style>
+@import url("../node_modules/reveal.js/dist/reveal.css");
+@import url("../node_modules/reveal.js/dist/theme/white.css");
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+  height: 100vh;
+}
+</style>
+
+// slightly adapted from @bricksroo App.vue //
+https://gist.github.com/bricksroo/4652f3c37b280e6f977890fb530893de
